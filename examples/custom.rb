350b96b5f0a8ed5d19d819309ed1d1dcaaa54ce2
//de <de@freed.network>
2015-06-17 20:11:31 +0000

    no need for this ... will just make an actual custom instance for `filter`

--- examples/custom.rb
+++
@@ -1,7 +0,0 @@
-$LOAD_PATH.push(File.expand_path("../../lib", __FILE__))
-
-require 'bundler/setup'
-require 'celluloid/smtp'
-
-Celluloid::SMTP::Logger = Celluloid[:logger] #de Use your own logging actor.
-
