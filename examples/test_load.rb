c331331e7d8558877a5147a1f7ef4eccfe5d35a3
//de <code@extremist.digital>
2015-06-19 11:13:55 -0700

    tweaked load tester

--- examples/test_load.rb
+++ examples/test_load.rb
@@ -60,28 +60,19 @@ values = tests.map(&:value)
 total = Time.now-start
 total_fail = false
 
-puts "\n\n\n"
-
-puts "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+puts "\n\n\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
 failures = values.map { |v| v[1] }.inject{ |sum,f| sum + f }
 average = values.map { |v| v[0] }.inject{ |sum, t| sum + t }.to_f / values.size
 overall_average = total/(TESTS*THREADS)
-puts ""
-if failures == THREADS*TESTS
-  puts "    All #{THREADS*TESTS} messages failed."
+if total_fail
+  puts "\n    All #{THREADS*TESTS} messages failed."
   total_fail = true
-else
-  puts "    Failures: #{failures}"
-  puts ""
-  puts "    Average time for #{TESTS}x#{THREADS} messages:"
-  puts "      ~#{"%0.4f" % average} seconds per message, actual estimate." unless total_fail
-  puts "      ~#{"%0.4f" % (overall_average)} of overall runtime, per message."
-  puts ""
 end
-puts "    Total time running test: #{"%0.4f" % total} seconds:"
+puts "\n    Failures: #{failures}"
+puts "\n    Average time for #{TESTS}x#{THREADS} messages:"
+puts "      ~#{"%0.4f" % average} seconds per message, actual estimate." unless total_fail
+puts "      ~#{"%0.4f" % (overall_average)} of overall runtime, per message."
+puts "\n    Total time running test: #{"%0.4f" % total} seconds:"
 puts "      ~#{"%0.4f" % (1/average)} messages per second, actual estimate." unless total_fail
 puts "      ~#{"%0.4f" % (1/overall_average)} messages per second, overall."
-puts ""
-puts "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
-puts "\n\n\n"
-
+puts "\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n\n\n"
