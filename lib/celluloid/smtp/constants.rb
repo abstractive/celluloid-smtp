5c299d67bffd3916860346fea71b69305469d0f7
//de <code@extremist.digital>
2015-06-20 22:00:22 -0700

    turn off event debugging

--- lib/celluloid/smtp/constants.rb
+++ lib/celluloid/smtp/constants.rb
@@ -2,7 +2,7 @@ module Celluloid::SMTP::Constants
   DEBUG = false
   DEBUG_TIMING = true
   DEBUG_AUTOMATA = false
-  DEBUG_EVENTS = true
+  DEBUG_EVENTS = false
 
   HANDLERS = 4
   LOGGER = Celluloid::Internals::Logger
